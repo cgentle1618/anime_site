1b936764d26371ee2ccf1c23b1f555b10d58985a
cgentle1618 <cgentle1618@gmail.com>
2026-04-12 20:56:07 +0800

    refactor(docker): revert temporary change to reset alembic in cloud

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-set -e
+set -e  # Exit immediately if a command fails
 
-echo "🚀 Performing one-time Alembic sync..."
-# This marks the DB as 'up to date' without running the failing SQL
-alembic stamp head 
+echo "🚀 Running Alembic Migrations..."
+alembic upgrade head
 
-echo "✨ Starting FastAPI..."
+echo "✨ Starting Uvicorn..."
 exec uvicorn main:app --host 0.0.0.0 --port ${PORT:-8080} --proxy-headers --forwarded-allow-ips='*'
