ea447a6c371b0d0ffa61d468efb2171537c0f8fa
cgentle1618 <cgentle1618@gmail.com>
2026-04-24 12:44:17 +0800

    order(anime): reorder fields of naming card in anime page

--- frontend/src/components/NamingCard.jsx
+++ frontend/src/components/NamingCard.jsx
@@ -4,9 +4,9 @@ export default function NamingCard({ cn, en, alt, jp, roman }) {
   const fields = [
     { label: "Chinese", value: cn },
     { label: "English", value: en },
-    { label: "Alternative", value: alt },
     { label: "Japanese", value: jp },
     { label: "Roman", value: roman },
+    { label: "Alternative", value: alt },
   ];
   return <InfoCard title="Naming" icon="fa-language" fields={fields} />;
 }
